c2ecafc3be64ea1c85217527cc5a149dca365d46
Shs0160 <aladinsue@gmail.com>
2024-02-17 23:09:15 +0900

    jdbc Template

--- src/main/java/b_project/spring_project/SpringConfig.java
+++ src/main/java/b_project/spring_project/SpringConfig.java
@@ -1,16 +1,27 @@
 package b_project.spring_project;
 
+import b_project.spring_project.repository.JdbcMemberRepository;
+import b_project.spring_project.repository.JdbcTemplateMemberRepository;
 import b_project.spring_project.repository.MemberRepository;
 import b_project.spring_project.repository.MemoryMemberRepository;
 import b_project.spring_project.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * 컴포넌트 스캔 말고 직접 스프링 빈 등록
  */
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     //스프링 빈 등록
     @Bean
@@ -21,6 +32,6 @@ public class SpringConfig {
     //이런식으로 직접 설정하면 나중에 저장소를 DB로 연결할 때 이 코드만 수정해주면 .
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
